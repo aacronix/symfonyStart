a344d22d858ab69bac66832f7c9de2d9cae8b797
Unknown <bacik.vas@gmail.com>
2017-10-19 15:45:35 +0300

    Довёл до 0.

--- src/AppBundle/Controller/BlogController.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: artem.vasilev
- * Date: 10/12/2017
- * Time: 10:02 PM
- */
--- src/AppBundle/Entity/Task.php
+++ src/AppBundle/Entity/Task.php
@@ -1,7 +1,30 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: artem.vasilev
- * Date: 10/18/2017
- * Time: 9:47 PM
- */
+
+// src/AppBundle/Entity/Task.php
+namespace AppBundle\Entity;
+
+class Task
+{
+    protected $task;
+    protected $dueDate;
+
+    public function getTask()
+    {
+        return $this->task;
+    }
+
+    public function setTask($task)
+    {
+        $this->task = $task;
+    }
+
+    public function getDueDate()
+    {
+        return $this->dueDate;
+    }
+
+    public function setDueDate(\DateTime $dueDate = null)
+    {
+        $this->dueDate = $dueDate;
+    }
+}
